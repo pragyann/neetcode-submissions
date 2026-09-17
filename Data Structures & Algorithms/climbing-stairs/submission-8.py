bb486c20ce9edea2556426370304dcf04c15190a
pragyann <70948847+pragyann@users.noreply.github.com>
2026-09-17 23:01:56 +1000

    Add: climbing-stairs - submission-8

---
+++ Data Structures & Algorithms/climbing-stairs/submission-8.py
@@ -0,0 +1,10 @@
+# Bottom up approach
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        dp = [1] * (n + 1) # [0, 1, 2, 3, 4, 5]
+
+        a, b = 1, 1
+
+        for _ in range(n-1):
+            a, b = b, a+b
+        return b
