6808fdc35a8d311ca96b40776291e8a1b4828f7d
pragyann <70948847+pragyann@users.noreply.github.com>
2026-09-17 23:02:14 +1000

    Add: climbing-stairs - submission-9

---
+++ Data Structures & Algorithms/climbing-stairs/submission-9.py
@@ -0,0 +1,9 @@
+# Bottom up approach, optimal
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        a, b = 1, 1
+
+        for _ in range(n-1):
+            a, b = b, a+b
+
+        return b
